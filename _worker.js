9901da579a31a30a8e270fe84d95be97dd0cb61f
guxiangbin <72550287+guxiangbin@users.noreply.github.com>
2024-07-26 23:02:12 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '3037872e-45aa-40b0-b314-0c1c6fbb4119';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
